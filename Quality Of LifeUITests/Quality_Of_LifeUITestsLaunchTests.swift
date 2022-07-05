5dfd6d60d0ac59ef1fb6dd78140f7ebb0dd07651
Dan Mori <danielstelzer@gmail.com>
2022-07-05 20:33:34 -0300

    Initial Commit

---
+++ Quality Of LifeUITests/Quality_Of_LifeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Quality_Of_LifeUITestsLaunchTests.swift
+//  Quality Of LifeUITests
+//
+//  Created by Dan Mori on 05/07/22.
+//
+
+import XCTest
+
+class Quality_Of_LifeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
